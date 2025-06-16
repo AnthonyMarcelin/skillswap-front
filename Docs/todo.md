1621c2e00c5484526aad96baee02614d25bd7432
Ludovic Fremaut <ludovic.fremaut@hotmail.fr>
2025-06-16 16:20:57 +0200

    feat : fetch skills allPages && todoList

---
+++ Docs/todo.md
@@ -0,0 +1,14 @@
+# A mettre en place
+
+- afficher quatre compétences (popular si assez de temps)
+- au click sur une "BubbleProfile" (nos derniers inscrits) de la homePage = être redirigé sur la profilepage/{id}.
+- créer un toast qui indique que vous avez séléctionné déjà trois compétences
+- Rendre cliquable la ProfileCard cliquable. Il faut etre redirigé vers la PRofilePage/{id} : Question ? profilecard entierement cliquable ou création d'un Bouton ?
+- au clic (button voir mon profil(SearchPage), button voir le profil de (HomePage)) être redigiré vers profilepage/{id}
+- PersonalPage a finir (en cours Karine && Antho)
+- css en deux colonnes sur la modal choix des compétences
+- Mode Desktop a mettre en place CSS
+
+# A améliorer
+- ProfileCard.tsx : Faire en sorte que les skills affichées ne soient pas coupées".
+- Page 404 (notFound à améliorer design)
--- src/hooks/useUserSearch.ts
+++ src/hooks/useUserSearch.ts
@@ -11,17 +11,18 @@ export function useUserSearch(initialUsers: IUser[]) {
     console.log("Recherche lancée :", skill, zipcode, usersToFilter);
 
     setFilteredUsers(
-
       usersToFilter.filter((user) => {
-  const hasSkill = user.skills.some((s) => s.name.trim().toLowerCase() === skill.trim().toLowerCase());
-  const hasZip = user.zipcode.trim() === zipcode.trim();
-  console.log(user.firstname, "hasSkill:", hasSkill, "hasZip:", hasZip);
-  if (skill && zipcode) return hasSkill && hasZip;
-  if (skill) return hasSkill;
-  if (zipcode) return hasZip;
-  return true;
-})
+        const hasSkill = user.skills.some(
+          (s) => s.name.trim().toLowerCase() === skill.trim().toLowerCase()
+        );
+        const hasZip = user.zipcode.trim() === zipcode.trim();
+        console.log(user.firstname, "hasSkill:", hasSkill, "hasZip:", hasZip);
+        if (skill && zipcode) return hasSkill && hasZip;
+        if (skill) return hasSkill;
+        if (zipcode) return hasZip;
+        return true;
+      })
     );
   }
   return { filteredUsers, handleSearch, setFilteredUsers };
-  }
+}
