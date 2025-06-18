9da8ab4ee4b97e002a29e67e0f16a2d55936ae6f
AnthonyMarcelin <anthony.marcelin13@gmail.com>
2025-06-18 12:03:37 +0200

    chore: wip: link to message page in header

--- src/pages/PersonalPage.tsx
+++ src/pages/PersonalPage.tsx
@@ -3,14 +3,12 @@ import Header from "@/components/Header";
 import { logout } from "@/services/auth.service";
 
 export default function PersonalPage() {
-
   const handleLogout = async () => {
     try {
       await logout();
-      console.log("Déconnexion réussie:", "cookie supprimé" );
-      window.location.href = '/register';
-}
-    catch (error) {
+      console.log("Déconnexion réussie:", "cookie supprimé");
+      window.location.href = "/register";
+    } catch (error) {
       console.error("Erreur lors de la déconnexion:", error);
       // Vous pouvez gérer l'erreur ici, par exemple en affichant un message à l'utilisateur
     }
@@ -21,7 +19,9 @@ export default function PersonalPage() {
       <Header />
 
       <main className="p-4 text-center">
-        <h1 className="text-xl font-semibold mb-4">Bienvenue sur ta page personnelle</h1>
+        <h1 className="text-xl font-semibold mb-4">
+          Bienvenue sur ta page personnelle
+        </h1>
         <button
           onClick={handleLogout}
           className="rounded bg-red-500 text-white px-4 py-2 hover:bg-red-600"
